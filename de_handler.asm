9984d0b7e3e3d8850c331258e788e265d4b59d40
Mor Paz <1912.mor@gmail.com>
2020-12-12 16:48:18 +0200

    begin work on q2

--- de_handler.asm
+++ de_handler.asm
@@ -6,4 +6,38 @@
 .text
 .align 4, 0x90
 my_de_handler:
-  #STUDENT NEED TO FILL
+  # prologue
+  push %rbp
+  movq %rsp, %rbp
+  # backing up the users' register values
+  push %rdi
+  push %rsi
+  push %rdx
+  push %rcx
+  push %r8
+  push %r9
+  # if we are here, that means a divide by zero exception has occured, we need to try and send the divided part to what_to_do
+  # since the issue is with divison, the divided is in rax
+  movq %rax, %rdi
+  # need to check if the divided is 0
+  cmp $0, %rdi
+  je old_handler
+
+  call what_to_do
+
+  # restore user register values
+  push %r9
+  push %r8
+  push %rcx
+  push %rdx
+  push %rsi
+  push %rdi
+  jmp return_from_what
+old_handler:
+    # the old rax is 0, which means we tried to perform 0/0, letting cpu handle normally
+    leave
+    jmp* old_de_handler
+
+return_from_what:
+    leave
+    iretq
--- de_utils.c
+++ de_utils.c
@@ -1,25 +1,19 @@
 #include <asm/desc.h>
 
 void my_store_idt(struct desc_ptr *idtr) {
-//# <STUDENT FILL>
-	// TODO: if we omit inline assembly:  store_idt(&tmpidtr);
-// </STUDENT FILL>
+    // TODO: if we omit inline assembly:  store_idt(&tmpidtr);
+    asm volatile("sidt %0" : "=m"(idtr));
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL>
-	// if we omit inline assembly: load_idt(addr);
-// <STUDENT FILL>
+    // if we omit inline assembly: load_idt(addr);
+    asm volatile( "lidt %0" : : "m"(idtr));
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
-// <STUDENT FILL>
-	// TODO: pack_gate(gate, GATE_INTERRUPT, addr, 0, 0, __KERNEL_CS);
-// </STUDENT FILL>
+    pack_gate(gate, GATE_INTERRUPT, addr, 0, 0, __KERNEL_CS);
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
-// <STUDENT FILL>
-	// TODO: return gate_offset(gate);
-// </STUDENT FILL>
+    return gate_offset(gate);
 }
