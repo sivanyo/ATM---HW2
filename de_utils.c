95c6168aa6be659a2ff6d5f8c2ff66175b84a7ad
Mor Paz <1912.mor@gmail.com>
2020-12-12 17:16:32 +0200

    update q2

--- de_utils.c
+++ de_utils.c
@@ -3,13 +3,13 @@
 void my_store_idt(struct desc_ptr *idtr) {
     // TODO: if we omit inline assembly:  store_idt(&tmpidtr);
     //asm volatile("sidt %0" : "=m"(idtr));
-    __asm__ __volatile__ ("sidt %0"; : "=m"(idtr));
+    __asm__ __volatile__ ("sidt %0;" : "=m"(idtr));
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
     // if we omit inline assembly: load_idt(addr);
     //asm volatile( "lidt %0" : : "m"(idtr));
-    __asm__ __volatile__ ("lidt %[idtptr]"; : :[idtptr]"m"(idtr));
+    __asm__ __volatile__ ("lidt %[idtptr];" : :[idtptr]"m"(idtr));
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
