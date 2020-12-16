2c5c9e35826d298322b23d5679f0689059cb7977
Mor Paz <1912.mor@gmail.com>
2020-12-16 20:00:44 +0000

    update

--- de_handler.asm
+++ de_handler.asm
@@ -10,58 +10,59 @@ my_de_handler:
   push %rbp
   movq %rsp, %rbp
   # backing up all the registers that have been in use at calc.c
-  push %rdi
-  push %rsi
-  push %rbx
-  push %rdx
-  push %r8
-  push %r9
-  push %r10
-  push %r11
-  push %r12
-  push %r13
-  push %r14
-  push %r15
+  pushq %rdi
+  pushq %rsi
+  pushq %rbx
+  pushq %rdx
+  pushq %r8
+  pushq %r9
+  pushq %r10
+  pushq %r11
+  pushq %r12
+  pushq %r13
+  pushq %r14
+  pushq %r15
   # if we are here, that means a divide by zero exception has occured, we need to try and send the divided part to what_to_do
   # since the issue is with divison, the divided is in rax
   movq %rax, %rdi
   call what_to_do
   cmp $0, %rax
   # the result of what to do is 0, so we let the old handler handle
-  je old_hanlder
+  je old_handler
   # now rax holds the val of what to do
   # set rcx to be 1, so the result of the division will be rax
   movq $1, %rcx
   # restore user register values
-  pop %r15
-  pop %r14
-  pop %r13
-  pop %r12
-  pop %r11
-  pop %r10
-  pop %r9
-  pop %r8
-  pop %rdx
-  pop %rbx
-  pop %rsi
-  pop %rdi
-  jmp return_from_what
+  popq %r15
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %r11
+  popq %r10
+  popq %r9
+  popq %r8
+  popq %rdx
+  popq %rbx
+  popq %rsi
+  popq %rdi
+  cqo
+  leave
+  iretq
+
+
 old_handler:
 # the old rax is 0, which means we tried to perform 0/0, letting cpu handle normally
-      pop %r15
-      pop %r14
-      pop %r13
-      pop %r12
-      pop %r11
-      pop %r10
-      pop %r9
-      pop %r8
-      pop %rdx
-      pop %rbx
-      pop %rsi
-      pop %rdi
-      jmp *old_de_handler
+      popq %r15
+      popq %r14
+      popq %r13
+      popq %r12
+      popq %r11
+      popq %r10
+      popq %r9
+      popq %r8
+      popq %rdx
+      popq %rbx
+      popq %rsi
+      popq %rdi
+      jmp * old_de_handler
 
-return_from_what:
-    leave
-    iretq
